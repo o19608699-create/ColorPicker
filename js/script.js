2eb5401140811e538e336a2d5540cc723c934b13
Hayaguri Ai <guri.6025@HayagurinoMacBook-Air.local>
2025-10-26 14:46:29 +0900

    update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 text.textContent = `カラーコード：${color.value}`;
 const colorBg =() =>{
+    //選択した色を背景色に設定
     document.body.style.backgroundColor = color.value;
+    //カラーコードを表示
     text.textContent = `カラーコード：${color.value}`};
+    //カラーピッカーが変更されたらcolorBgを発動させる
 color.addEventListener('input',colorBg);
